33f0c458cd0093c6acbcb3c8a2934f05b35d115b
Ibrahim Mohamed <Ibrahim_20210007@fci.helwan.edu.eg>
2025-10-13 04:22:11 +0300

    feat: make the bg of mulch and soil pages changes dynamiclly

--- src/app/components/soilAndMulchPageSections/Hero.tsx
+++ src/app/components/soilAndMulchPageSections/Hero.tsx
@@ -3,7 +3,10 @@ import { useTranslations } from "next-intl";
 export function Hero({ title }: { title: string }) {
   const t = useTranslations("informationMulchAndSoil.hero");
   return (
-    <div className="bg-[url('/mulchAndSoil/hero.png')] bg-cover w-dvw h-dvh p-[var(--section-Padding)]  flex items-end justify-start relative">
+    <div
+      className=" bg-cover w-dvw h-dvh p-[var(--section-Padding)]  flex items-end justify-start relative"
+      style={{ backgroundImage: `url(/mulchAndSoil/${title}-hero.png)` }}
+    >
       <p className=" text-[5rem] font-black text-white leading-[6.75rem]">
         {t(title)}
       </p>
